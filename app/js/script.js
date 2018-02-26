2c70e2eaa8df6daa5879d3035c758865ecaa328b
slaus <mister.slaus@gmail.com>
2018-02-26 21:53:52 +0200

    Add files via upload

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,12 @@
-jQuery(function ($) {
-    $('.auto-save').savy('load');
-});
+// jQuery(function($) {
+//     $(".auto-save").savy("load");
+
+//     if ($("input").val() == "") { $("#step-1 .btn.nextBtn").attr("disabled", true); }
+
+//     //$("#step-1 select").val();
+//     $("#step-1 select").on("change", function() {
+//         var val = $(this).val();
+//         if (val !== "") { $("#step-1 .btn.nextBtn").attr("disabled", false); }
+//         console.log(val);
+//     });
+// });
